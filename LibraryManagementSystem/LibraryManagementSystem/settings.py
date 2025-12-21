6028084b7211ca956ec8a9dab3d2a199d63350fa
MithunKumarRajak <mithunkumarrajak01012005@gmail.com>
2025-12-21 17:33:20 +0530

    Add WhiteNoise middleware for static file handling and update carousel image alt texts

--- LibraryManagementSystem/LibraryManagementSystem/settings.py
+++ LibraryManagementSystem/LibraryManagementSystem/settings.py
@@ -49,7 +49,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 ROOT_URLCONF = 'LibraryManagementSystem.urls'
 
